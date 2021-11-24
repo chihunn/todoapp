2cc6132408d6caf6258024443a83ee3c93c261fd
chihunn <sch804@pukyong.ac.kr>
2021-11-25 00:48:49 +0900

    Take Google Calendar

---
+++ js/calendar.js
@@ -0,0 +1,17 @@
+import Calendar from "tui-calendar";
+import "tui-calendar/dist/tui-calendar.css";
+
+import "tui-date-picker/dist/tui-date-picker.css";
+import "tui-time-picker/dist/tui-time-picker.css";
+
+let calendar = new Calendar("#calendar", {
+  defaultView: "month",
+  taskView: true,
+  template: {
+    monthDayname: function (dayname) {
+      return (
+        '<span class="calendar-week-dayname-name">' + dayname.label + "</span>"
+      );
+    },
+  },
+});
